7d35fa03b3954536b1ff73098275cb1632bbadac
NeonLt <viliusburzdzius@gmail.com>
2022-11-04 19:08:01 +0200

    first commit

---
+++ app/src/main/java/com/example/database/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.database;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        //admin and admin
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    //correct;
+                    //openContextMenu("Activity2"   );
+
+                } else
+                    //incorrect
+                    Toast.makeText(MainActivity.this, "LOGIN FAILED", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/database/register.java
@@ -0,0 +1,19 @@
+package com.example.database;
+
+import android.content.SharedPreferences;
+
+public class register {
+    private String usernameLogin, usernameRegister, password, email;
+    private static final String PREFERENCES_FILE_NAME = "com.example.svecias.helloworld";
+    private static final String USERNAME_KEY = "username";
+    private static final String PASSWORD_KEY = "password";
+    private static final String REMEMBER_ME_KEY = "rememberMe";
+    private SharedPreferences sharedPrefs;
+
+    //naudojamas registruojant nauja vartotoja
+    //public User(String usernameRegister, String password, String email){
+        //    this.usernameRegister = usernameRegister;
+        //    this.password = password;
+         //   this.email  = email;
+   //}
+}
